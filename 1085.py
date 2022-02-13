fea26b478804c9079d3f3da49aaf413fb841a3e5
정재호 <b8739@hanmail.net>
2022-02-13 16:09:49 +0900

    After: 직사각형에서 탈출

--- 1085.py
+++ 1085.py
@@ -1,9 +1,7 @@
-p = []
-x,y,w,h = map(int, input().split())
+x, y, w, h = map(int, input().split())
+print(min(x, y, w-x, h-y))
 
-p.append(abs(0 - x))
-p.append(abs(x - w))
-p.append(abs(0 - y))
-p.append(abs(y - h))
+#변경사항
 
-print(min(p))
+# 왼쪽 모서리 (0,0)간의 거리는 x,y,에서 0을 빼는 거기 때문에 x-0, y-0 이렇게 할 필요 없이, 그냥 x,y 사용
+# 값을 넣어두지 않고 print로 바로 값 출력
