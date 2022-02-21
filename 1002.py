9aaf20073ed5a29ed1252409cf93f0ff91971359
정재호 <b8739@hanmail.net>
2022-02-21 11:20:04 +0900

    10870 피보나치 5

--- 1002.py
+++ 1002.py
@@ -5,6 +5,7 @@ for _ in range(t):
     x1, y1, r1, x2, y2, r2 = map(int,input().split())
     
     d = math.sqrt((x1-x2)**2+(y1-y2)**2)
+    #if랑 elif 순서 바뀌면 안되는데 왜 인지 알아볼것
     if d==0 and r1==r2:
         print(-1)   
     elif abs(r2-r1)==d or r1+r2==d :
