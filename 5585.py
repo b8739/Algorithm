b77d5ed6749c0a3c6d2158230a108dbded572e94
정재호 <b8739@hanmail.net>
2022-02-23 17:08:15 +0900

    5585 코드 간결하게 수정

--- 5585.py
+++ 5585.py
@@ -1,4 +1,5 @@
 #거스름돈
+#수정 전
 money = 1000 - int(input())
 change = [500,100,50,10,5,1]
 cnt = 0
@@ -11,3 +12,12 @@ for c in change:
         cnt += money//c #3
         money = money%c #100
 print(cnt)
+
+#수정 후
+a = 1000 - int(input())
+b = [500, 100, 50, 10, 5, 1]
+count = 0
+for i in b:
+    count += a // i
+    a %= i
+print(count)
