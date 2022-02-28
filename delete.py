b41c11e785c1e46913b1f13c38e841ea4e873d85
정재호 <b8739@hanmail.net>
2022-02-28 16:46:22 +0900

    3273번 두 수의 합

--- delete.py
+++ delete.py
@@ -1,31 +1,38 @@
 import sys
 
-def dfs(x):
-    if x >= n: 
-        return False
+input = sys.stdin.readline
+
+n,t = map(int,input().split())
+res = 0
+
+parents = [i for i in range(n)]
+
+def find(x):
+    if x == parents[x]:
+        return x
+    parents[x] = find(parents[x])
+    return parents[x]
+
+# def union(a,b):
+#     a = find(a)
+#     b = find(b)
+#     if a!=b:
+#         parents[max(a,b)] = min(a,b)
+#         return False
+#     else:
+#         return True
+def union(x, y, idx):
+    global res
+    x = find(x)
+    y = find(y)
+    if x != y:
+        parents[max(x,y)] = min(x,y)
+    elif res == 0:
+        res = idx
+
+
+for idx in range(t):
+    a,b = map(int,input().split())
+    union(a,b,idx)
     
-    if visited[x]==False:
-        visited[x] = True
-        for i in graph[x]:     
-                dfs(i)           
-        return True
-    return False
-
-#True가 될 때마다 1을 더한다
-n,m = map(int,input().split()) # [[],[2,3],[1,4]]
-
-visited = [False] * (n + 1)
-
-graph = [[] for _ in range(n+1)] #0 포함해서 생성
-
-for i in range(m):
-    a,b = map(int,sys.stdin.readline().split()) # 1,2
-    graph[a].append(b)
-    graph[b].append(a)
-    
-sum = 0
-
-for i in range(1, n+1):
-        if dfs(i) == True:
-            sum+=1
-print(sum)
+print(res)
