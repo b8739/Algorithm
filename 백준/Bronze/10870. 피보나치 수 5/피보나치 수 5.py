763462634725786b7a6b8c907a528d33e5392c15
정재호 <48551743+b8739@users.noreply.github.com>
2022-03-09 15:49:09 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,10 @@
+def getPivonachi(n):
+    pivo = [0,1]
+    for _ in range(n-1):
+        pivo.append(pivo[-1]+pivo[-2])
+#     print(pivo)
+    return pivo[n]
+
+n = int(input())
+
+print(getPivonachi(n))
