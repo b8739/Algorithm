a08ecd9de4ac5cddf721fb2049a7cf6921ae86e9
정재호 <48551743+b8739@users.noreply.github.com>
2022-03-09 16:02:06 +0900

    [Silver II] Title: 회의실 배정, Time: 4496 ms, Memory: 53684 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,23 @@
+#끝나는 시간은 무조건 시작 시간보다 크므로, 끝나는 시간이 낮은거 기준으로 해야함
+#시작 시간이 같다면, 끝나는 시간이 낮은게 좋음
+
+n = int(input())
+time = [[0]*2 for _ in range(n)]
+
+cnt = 1
+
+for i in range(n):
+    s,e= map(int,input().split())
+    time[i][0] = s
+    time[i][1] = e   
+#time = [[s,e],[s,e],[s,e],[s,e]]
+
+time.sort(key = lambda x: (x[1], x[0]))
+
+end = time[0][1]
+
+for i in time[1:]:
+    if end <= i[0]:
+        end = i[1]
+        cnt+=1
+print(cnt)    
