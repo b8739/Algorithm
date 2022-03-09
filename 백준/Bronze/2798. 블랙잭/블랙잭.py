f0fb491ed3482a1ea029e7bc96392cac5f9ca34a
정재호 <48551743+b8739@users.noreply.github.com>
2022-03-09 16:07:56 +0900

    [Bronze II] Title: 블랙잭, Time: 144 ms, Memory: 30860 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,16 @@
+from itertools import combinations
+
+n,target = map(int,input().split())
+
+nums = map(int,input().split()) 
+
+max = 0
+
+for i in combinations(nums,3):
+    sum = 0
+    for j in i:
+        sum+=j
+    if sum>max and sum<=target:
+        max=sum
+        
+print (max)
