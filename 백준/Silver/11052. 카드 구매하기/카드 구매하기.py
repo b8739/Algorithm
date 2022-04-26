ab9df00600982a3cdd59aada24bcda74838b78b5
정재호 <48551743+b8739@users.noreply.github.com>
2022-04-26 22:29:13 +0900

    [Silver I] Title: 카드 구매하기, Time: 208 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -1,29 +1,14 @@
+
 n = int(input())
 
 s = [0] + list(map(int,input().split()))
+dp = [0]+ [0 for _ in range(n+1)]
 
-dp = [0]
-
-dp.append(s[1])
 
 def sol(dp):
-    if n > 1:
-        dp.append(max(dp[1] * 2, s[2]))    
-    if n < 2:
-        print(dp[n])
-        return
-    
-
-    for i in range(3,n+1):
-        maxv = s[i]
-
-        for j in range(1,i):
-            if i % j == 0:
-                maxv = max(maxv,dp[j] * i//j)
-            else:
-                maxv = max(maxv,dp[j] + dp[i-j])
-
-        dp.append(maxv)
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            dp[i] = max(dp[i],dp[i-j] + s[j])
     print(dp[n])
 
-sol(dp)       
+sol(dp)        
